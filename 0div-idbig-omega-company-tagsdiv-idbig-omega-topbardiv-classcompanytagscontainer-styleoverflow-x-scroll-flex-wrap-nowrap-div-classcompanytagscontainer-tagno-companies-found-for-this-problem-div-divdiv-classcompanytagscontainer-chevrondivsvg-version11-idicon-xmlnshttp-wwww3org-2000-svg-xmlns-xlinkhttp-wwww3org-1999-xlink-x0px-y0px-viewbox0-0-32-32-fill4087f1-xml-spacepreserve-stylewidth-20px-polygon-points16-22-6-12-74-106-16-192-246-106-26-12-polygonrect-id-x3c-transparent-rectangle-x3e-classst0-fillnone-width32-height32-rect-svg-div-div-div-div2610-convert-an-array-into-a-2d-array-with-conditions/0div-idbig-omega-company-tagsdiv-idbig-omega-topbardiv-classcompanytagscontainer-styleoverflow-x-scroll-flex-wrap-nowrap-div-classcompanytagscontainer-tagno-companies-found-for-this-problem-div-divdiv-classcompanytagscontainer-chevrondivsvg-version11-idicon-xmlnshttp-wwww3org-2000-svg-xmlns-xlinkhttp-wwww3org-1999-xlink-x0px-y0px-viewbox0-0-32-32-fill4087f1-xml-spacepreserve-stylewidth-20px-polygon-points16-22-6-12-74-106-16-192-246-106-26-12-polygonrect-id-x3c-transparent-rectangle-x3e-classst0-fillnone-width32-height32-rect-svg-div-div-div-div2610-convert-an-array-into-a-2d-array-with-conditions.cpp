ca84e34ca753769e212a8e0cfcb869708c6104d3
aryan678 <72307273+aryan678@users.noreply.github.com>
2024-01-02 22:32:07 +0530

    Time: 23 ms (9.84%), Space: 29.8 MB (83.60%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2610-convert-an-array-into-a-2d-array-with-conditions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> freq(nums.size() + 1);
+        
+        vector<vector<int>> ans;
+        for (int i=0;i<nums.size();i++) {
+            if (freq[nums[i]] >= ans.size()) {
+                ans.push_back({});
+            }
+            
+            // Store the integer in the list corresponding to its current frequency.
+            ans[freq[nums[i]]].push_back(nums[i]);
+            freq[nums[i]]++;
+        }
+        
+        return ans;
+    }
+};
